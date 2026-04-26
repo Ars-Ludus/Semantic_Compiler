191c9089c0ee9ca5653a3db6f5ad75f6134c53e7
Ars <mcnpandroid@gmail.com>
2026-04-26 11:06:59 -0500

    feat(personal): define Token struct

--- semcom_personal/personal.go
+++ semcom_personal/personal.go
@@ -7,3 +7,10 @@ import (
 // Schema is the SQL schema for the personal module.
 //go:embed schema.sql
 var Schema string
+
+// Token represents a personal token.
+type Token struct {
+	ID   uint32
+	Word string
+	Type string
+}
